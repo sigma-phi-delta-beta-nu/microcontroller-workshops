bc1719544c724a58ea15cc7aa670d88562c85ab5
Brandon Kelley <14bmkelley@gmail.com>
2016-01-10 23:12:32 -0800

    adding the dim program

---
+++ week1/dim.c
@@ -0,0 +1,32 @@
+#include <msp430.h>
+
+int main() {
+  
+  // Constants
+  const int FREQ = 20;
+  
+  // Variables
+  int i = 0;
+  
+  // Setup
+  WDTCTL = WDTPW | WDTHOLD;
+  P1DIR |= BIT0;
+  
+  // Loop forever
+  while (1) {
+    
+    // Spend one cycle with output high
+    P1OUT |= BIT0;
+    __delay_cycles(100);
+    
+    // Spend remaining cycles with output low
+    P1OUT &= ~BIT0;
+    while (++i < FREQ) {
+      __delay_cycles(100);
+    }
+    
+  }
+  
+  return 0;
+  
+}
